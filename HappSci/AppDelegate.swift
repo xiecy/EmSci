a344fbac85253fc0e81ac4fff890cd6c3eb201bf
xiecy <xiecy_123@hotmail.com>
2016-11-13 03:57:38 -0500

    Initial Commit

---
+++ HappSci/AppDelegate.swift
@@ -0,0 +1,66 @@
+//
+//  AppDelegate.swift
+//  HappSci
+//
+//  Created by Clark on 11/12/16.
+//  Copyright © 2016 Clark Xie. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        let center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options:[.badge, .alert, .sound]) { (granted, error) in
+            if !granted {
+            print("Something went wrong")
+            }
+            // Enable or disable features based on authorization.
+        }
+        //application.registerForRemoteNotifications()
+        return true
+    }
+    
+
+/*
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        
+        let deviceTokenString = deviceToken.reduce("", {$0 + String(format: "%02X", $1)})
+        print(deviceTokenString)
+    }
+*/    
+    //http://stackoverflow.com/questions/37956482/registering-for-push-notifications-in-xcode-8-swift-3-0
+    //https://www.sitepoint.com/developing-push-notifications-for-ios-10/
+
+    func applicationWillResignActive(_ application: UIApplication) {
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+}
+
+
+
---
+++ HappSci/ViewController.swift
@@ -0,0 +1,140 @@
+//
+//  ViewController.swift
+//  HappSci
+//
+//  Created by Clark on 11/12/16.
+//  Copyright © 2016 Clark Xie. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var notifStateLabel: UILabel!
+    @IBOutlet weak var webView: UIWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        notifStateLabel.text = "\nEmSci"
+        
+        self.view.backgroundColor = UIColor.white
+        
+        let webUrl = URL(string: "http://emsci.net")
+        let requestObj = URLRequest(url: webUrl!)
+        webView?.loadRequest(requestObj)
+        
+        //webView?.scalesPageToFit = true
+        sendAutomatedNotification()
+        
+        //sendNotification(title: "aaa", body: "bbb")
+    }
+/*
+    @IBAction func sendPressed(_ sender: AnyObject) {
+        let content = UNMutableNotificationContent()
+        
+        content.title = "Ity's time to take your EmSci assessment!"
+        //content.body = "It's time to take your EmSci assessment!"
+        content.sound = UNNotificationSound.default()
+        
+        // Deliver the notification in five seconds.
+        //let trigger = UNTimeIntervalNotificationTrigger.init(timeInterval: 5, repeats: false)
+        
+        //let date = Date(timeIntervalSinceNow: 10)
+        
+        let hr = 18
+        let min = 43
+        let sec = 0.0
+        let hms = 18000.0 + Double(hr) * 3600.0 + Double(min) * 60.0 + Double(sec)
+        
+        let date = Date(timeIntervalSince1970: hms)
+        
+        /*
+        var date = [Date]()
+        date.append(Date(timeIntervalSince1970: hms))
+        date.append(Date(timeIntervalSince1970: hms + 5.0))
+        date.append(Date(timeIntervalSince1970: hms + 10.0))
+         */
+        // 18000 is 12:00 AM for EST
+        let triggerDaily = Calendar.current.dateComponents([.hour,.minute,.second,], from: date)
+        let trigger = UNCalendarNotificationTrigger(dateMatching: triggerDaily, repeats: true)
+        
+        let request = UNNotificationRequest.init(identifier: "reminder", content: content, trigger: trigger)
+        
+        // Schedule the notification.
+        let center = UNUserNotificationCenter.current()
+        center.add(request) { (error) in
+            print(error)
+        }
+        print("Sending a notification in 5 seconds")
+    }
+*/
+    func sendAutomatedNotification() {
+        let content = UNMutableNotificationContent()
+        
+        content.title = "EmSci Assessment"
+        content.body = "It's time to take your EmSci assessment!"
+        content.sound = UNNotificationSound.default()
+        
+        // Deliver the notification in five seconds.
+        //let trigger = UNTimeIntervalNotificationTrigger.init(timeInterval: 5, repeats: false)
+        
+        //let date = Date(timeIntervalSinceNow: 10)
+        
+        let hr = 19
+        let min = 46
+        let sec = 0.0
+        let hms = 18000.0 + Double(hr) * 3600.0 + Double(min) * 60.0 + Double(sec)
+        
+        let date = Date(timeIntervalSince1970: hms)
+        
+        /*
+         var date = [Date]()
+         date.append(Date(timeIntervalSince1970: hms))
+         date.append(Date(timeIntervalSince1970: hms + 5.0))
+         date.append(Date(timeIntervalSince1970: hms + 10.0))
+         */
+        // 18000 is 12:00 AM for EST
+        let triggerDaily = Calendar.current.dateComponents([.hour,.minute,.second,], from: date)
+        let trigger = UNCalendarNotificationTrigger(dateMatching: triggerDaily, repeats: true)
+        
+        let request = UNNotificationRequest.init(identifier: "reminder", content: content, trigger: trigger)
+        
+        // Schedule the notification.
+        let center = UNUserNotificationCenter.current()
+        center.add(request) { (error) in
+            print(error)
+        }
+        
+        print("Sending a notification at designated time")
+        //otifStateLabel.text = "Notification configured"
+    }
+    
+    
+    /*
+    func sendNotification(title: String, body: String) {
+        let content = UNMutableNotificationContent()
+        content.title = title
+        content.body = body
+        content.badge = 1
+        content.sound = UNNotificationSound.default()
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5,
+                                                        repeats: false)
+        
+        let notifRequest = UNNotificationRequest(identifier: "notification",
+                                                 content: content,
+                                                 trigger: trigger)
+    }
+ */
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+}
+
